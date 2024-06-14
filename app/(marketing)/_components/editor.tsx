7a344381310561c06f6a1dfb8f437fae550c7172
gordonwu66 <gordonwu66@gmail.com>
2024-06-14 01:30:49 -0400

    Blocknote editor

--- app/(marketing)/_components/editor.tsx
+++ app/(marketing)/_components/editor.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
 
 export const Editor = () => {
+    const editor: BlockNoteEditor = useCreateBlockNote();
     return (
-        <div>
-
+        <div className="w-[50%]">
+            <BlockNoteView editor={editor} theme="light" />
         </div>
     );
 }
